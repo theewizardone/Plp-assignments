1e2480e79c00b216e001e1a8c35d95f7935c54c2
Alfonce Morara <105839366+theewizardone@users.noreply.github.com>
2024-05-24 22:05:43 +0300

    Firstcommit

---
+++ python/user_input.py
@@ -0,0 +1,11 @@
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Ask the user for their age
+age = input("How old are you? ")
+
+# Ask the user for their location
+location = input("Where do you live? ")
+
+# Print out a personalized message
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
